0504ad9c0378cfebaa641804a463926713197054
Dremeret <terletskyyd2010@gmail.com>
2024-01-08 19:28:00 +0200

    First commit

---
+++ task-3.js
@@ -0,0 +1,38 @@
+
+// class Storage{
+    //  items;
+//     constructor(items){
+//      this.items = items;
+//     }
+//     getItems() {
+//    return this.item;
+//     }
+//     addItem(item) {
+//    this.items.push(item)
+//     }
+//     removeItem(item){
+// const index = this.items.indexOf(item);
+//      if(items !== -1){
+//       this.items.splice(1, 1);
+//      }
+//     }
+// }
+
+
+
+
+// const storage = new Storage([
+//     'Нанітоіди',
+//     'Пролонгер',
+//     'Залізні жупи',
+//     'Антигравітатор',
+//    ]);
+   
+//    const items = storage.getItems('');
+//    console.table(items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор" ]
+   
+//    storage.addItem('Дроїд');
+//    console.table(storage.items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор", "Дроїд" ]
+   
+//    storage.removeItem('Пролонгер');
+//    console.table(storage.items); // [ "Нанітоіди", "Залізні жупи", "Антигравітатор", "Дроїд" ]
